a43cda9d35642484a02af71c5279b7490d94bf24
Edu <eduardo.ex7@gmail.com>
2023-02-28 03:50:11 -0300

    Alt. 1

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{ useRef, useEffect, useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import LottieView from 'lottie-react-native'
 
 export default function App() {
+
+  const animation = useRef(null)
+  const [status, setStatus] = useState(false)
+
+  useEffect(() => {
+    if(status){
+      animation.current.play(0, 150)
+    }
+    else{
+      animation.current.play(150, 0)
+    }
+  }, [status])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity onPress={() => setStatus(!status)}>
+        <LottieView 
+          source={require('./src/Componentes/button-delete.json')}
+          autoPlay={false}
+          loop={false}
+          style={{width: 100, height: 300}}
+          resizeMode='cover'
+          ref={animation}
+        />
+      </TouchableOpacity>
     </View>
   );
 }
